8a1df61e29987ebbfd3b5a6c64187cda50ce67a9
KrasnikovEI <Krasnikovei@sibedge.com>
2020-03-10 17:29:53 +0700

    fix hh_parser

--- src/hh_parser.py
+++ src/hh_parser.py
@@ -13,22 +13,24 @@ class Parser:
         self.urls.append(self.base_url)
         session = requests.Session()
         request = session.get(url=self.base_url, headers=self.headers)
+        count = 0
 
         try:
             soup = bs(request.content, 'html.parser')
             pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
+
             if pagination:
                 count = int(pagination[-1].text)
-            else:
-                count = 0
+
             for i in range(count):
-                url = f'https://tomsk.hh.ru/search/vacancy?L_is_autosearch=false&area=90&clusters=true&enable_snippets=true&text=Python&page={i}'
+                url = f'https://hh.ru/search/vacancy?L_is_autosearch=false&area=113&clusters=true&enable_snippets=true&text=Python&page={i}'
                 if url not in self.urls:
                     self.urls.append(url)
         except ConnectionError as exc:
             print(exc)
 
     def parse(self):
+
         for url in self.urls:
             session = requests.Session()
             request = session.get(url=url, headers=self.headers)
@@ -37,9 +39,12 @@ class Parser:
 
             for div in divs:
                 try:
+                    if div is None:
+                        continue
                     title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
                     href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
                     company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                    city = div.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
                     text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
                     text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
                     context = f'{text1} {text2}'
@@ -50,6 +55,7 @@ class Parser:
                         'title': title,
                         'link': href,
                         'company': company,
+                        'city': city,
                         'context': context
                     })
 
@@ -62,7 +68,7 @@ def main():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
         'accept': '*/*',
     }
-    base_url = 'https://tomsk.hh.ru/search/vacancy?L_is_autosearch=false&area=90&clusters=true&enable_snippets=true&text=Python&page=0'
+    base_url = 'https://hh.ru/search/vacancy?L_is_autosearch=false&area=113&clusters=true&enable_snippets=true&text=Python&page=0'
 
     parser = Parser(headers, base_url)
     parser.get_urls()
@@ -71,7 +77,8 @@ def main():
     for job in jobs:
         print(job['title'])
         print(job['link'])
-        print(job['company'])
+        print(job['company'].strip(' '))
+        print(job['city'])
         print(job['context'])
         print('-----------------\n')
 
